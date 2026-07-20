e8a05a6ab4dbe371c48a076ce5f6e04cf7f91780
Oleh Yemelianov <emelianov2811@gmail.com>
2026-07-20 16:22:25 +0300

    Added script to extract video data from youtube

---
+++ test_extract/extract_video_data.py
@@ -0,0 +1,115 @@
+import requests
+import os
+import json
+from datetime import date
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("YT_API_KEY")
+CHANNEL_HANDLER = os.getenv("CHANNEL_HANDLER")
+MAX_RESULTS = 50
+
+def get_playlist_id():
+    url = f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLER}&key={API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+    json_data = json.dumps(data, indent=4)
+    # print(json_data)
+
+    try:
+        extract_playlist = data['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+    except (KeyError, IndexError) as e:
+        print(f"Unable to find playlist id: {e}")
+        raise
+
+    print(extract_playlist)
+    return extract_playlist
+
+
+def get_video_ids(playlist_id):
+    video_ids = []
+    pageToken = None
+    base_url = (f'https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={MAX_RESULTS}'
+                f'&playlistId={playlist_id}&key={API_KEY}')
+
+    try:
+        while True:
+            url = base_url
+
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+
+            response = requests.get(url)
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+
+            pageToken = data.get('nextPageToken')
+
+            if not pageToken:
+                break
+
+        return video_ids
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+
+def extract_video_data(video_ids):
+    extracted_data = []
+
+    def batch_list(video_id_list, batch_size):
+        for video_id in range(0, len(video_id_list), batch_size):
+            yield video_id_list[video_id: video_id + batch_size]
+
+    try:
+        for batch in batch_list(video_ids, MAX_RESULTS):
+            video_ids_str = ",".join(batch)
+            url = f'https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_ids_str}&key={API_KEY}'
+
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['id']
+                snippet = item['snippet']
+                content_details = item['contentDetails']
+                statistics = item['statistics']
+
+                video_data = {
+                    "video_id": video_id,
+                    "title": snippet['title'],
+                    "published_at": snippet['publishedAt'],
+                    "duration": content_details['duration'],
+                    "viewCount": statistics.get("viewCount", None),
+                    "likeCount": statistics.get("likeCount", None),
+                    "commentCount": statistics.get("commentCount", None),
+                }
+
+                extracted_data.append(video_data)
+
+        return extracted_data
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+
+def save_to_json(extracted_data):
+    file_path = f"./data/yt_data_{date.today()}.json"
+
+    with open(file_path, "w", encoding='utf-8') as json_outfile:
+        json.dump(extracted_data, json_outfile, indent=4, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    playlist_id = get_playlist_id()
+    video_ids = get_video_ids(playlist_id)
+    # print(extract_video_data(video_ids))
+    video_data = extract_video_data(video_ids)
+    save_to_json(video_data)
--- test_extract/video_stats.py
+++ test_extract/video_stats.py
@@ -6,9 +6,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+API_KEY = os.getenv("YT_API_KEY")
+CHANNEL_HANDLER = os.getenv("CHANNEL_HANDLER")
+
 def establish_connection():
-    API_KEY = os.getenv("YT_API_KEY")
-    CHANNEL_HANDLER = os.getenv("CHANNEL_HANDLER")
     url = f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLER}&key={API_KEY}'
 
     try:
@@ -34,8 +35,51 @@ def extract_data(response):
     return extract_playlist
 
 
+
+
+
+def extract_video_data(video_ids):
+    extracted_data = []
+
+    def batch_list(video_id_list, batch_size):
+        for video_id in range(0, len(video_id_list), batch_size):
+            yield video_id_list[video_id:video_id + batch_size]
+
+        try:
+            for batch in batch_list(video_ids, max_results):
+                video_ids_str = ",".join(batch)
+                url = f'https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_ids_str}&key={API_KEY}'
+
+                response = requests.get(url)
+                response.raise_for_status()
+                data = response.json()
+
+                for item in data.get('items', []):
+                    video_id = item['id']
+                    snippet = item['snippet']
+                    content_details = item['contentDetails']
+                    statistics = item['statistics']
+
+                    video_data = {
+                        "video_id": video_id,
+                        "title": snippet['title'],
+                        "published_at": snippet['published_at'],
+                        "duration": content_details['duration'],
+                        "viewCount": statistics.get("viewCount", None),
+                        "likeCount": statistics.get("likeCount", None),
+                        "commentCount": statistics.get("commentCount", None),
+                    }
+
+                    extracted_data.append(video_data)
+
+             return extracted_data
+
+        except requests.exceptions.RequestException as e:
+            raise e
+
+
 if __name__ == "__main__":
     response = establish_connection()
-    extract_data = extract_data(response)
+    result = extract_data(response)
 
 
