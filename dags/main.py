9bdb6bd3e47b009a619fb7280d236f98cb36cacf
Oleh Yemelianov <emelianov2811@gmail.com>
2026-07-27 14:49:12 +0300

    Modified DAGs with trigger dag operator

--- dags/main.py
+++ dags/main.py
@@ -6,6 +6,8 @@ from dataquality.soda import check_data_quality
 
 from datawarehouse.dwh import staging_table, core_table
 
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+
 local_tz = pendulum.timezone("Europe/Kyiv")
 
 default_args = {
@@ -31,38 +33,47 @@ with DAG(
     description="DAG to produce json file with youtube data",
     schedule="0 14 * * *",
     catchup=False
-) as dag:
+) as dag_produce:
 
     playlist_id = get_playlist_id()
     video_ids = get_video_ids(playlist_id)
     extract_data = extract_video_data(video_ids)
     save = save_to_json(extract_data)
 
+    trigger_update_db = TriggerDagRunOperator(
+        task_id="trigger_update_db",
+        trigger_dag_id="update_db",
+    )
 
-    playlist_id >> video_ids >> extract_data >> save
+
+    playlist_id >> video_ids >> extract_data >> save >> trigger_update_db
 
 
 with DAG(
     dag_id='update_db',
     default_args=default_args,
     description="DAG to proccess JSON file and insort date into staging and core schemas",
-    schedule="0 15 * * *",
+    schedule=None,
     catchup=False
-) as dag:
+) as dag_update:
 
     update_staging = staging_table()
     update_core = core_table()
 
-    update_staging >> update_core
+    trigger_data_quality = TriggerDagRunOperator(
+        task_id="trigger_data_check",
+        trigger_dag_id="data_check",
+    )
 
+    update_staging >> update_core >> trigger_data_quality
 
 with DAG(
     dag_id='data_check',
     default_args=default_args,
     description="DAG to execute data quality checks",
-    schedule="0 16 * * *",
+    schedule=None,
     catchup=False
-) as dag:
+) as dag_quality:
 
     data_quality_test_staging = check_data_quality(staging_schema)
     data_quality_test_core = check_data_quality(core_schema)
