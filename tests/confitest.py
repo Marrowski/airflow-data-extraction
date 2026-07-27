75cb57c4a97c3a80b43d95b23b7c984d959992f1
Oleh Yemelianov <emelianov2811@gmail.com>
2026-07-27 14:32:56 +0300

    Added integration test

--- tests/confitest.py
+++ tests/confitest.py
@@ -1,8 +1,9 @@
 import os
 import sys
 import pytest
+import psycopg2
 from unittest import mock
-from airflow.models import Variable, Connection,DagBag
+from airflow.models import Variable, Connection, DagBag
 
 @pytest.fixture
 def api_key():
@@ -14,7 +15,6 @@ def api_key():
 def channel_handle():
     with mock.patch.dict("os.environ", AIRFLOW_VAR_CHANNEL_HANDLER="VanillaImpact"):
         yield Variable.get("CHANNEL_HANDLER")
-
 
 
 @pytest.fixture
@@ -32,10 +32,46 @@ def mock_postgres_conn_vars():
         yield Connection.get_connection_from_secrets(conn_id="POSTGRES_CONN_USERNAME")
 
 
-
 @pytest.fixture
 def dagbag():
     dags_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'dags'))
     if dags_path not in sys.path:
         sys.path.insert(0, dags_path)
     yield DagBag()
+
+
+@pytest.fixture()
+def airflow_variable():
+    def get_airflow_variable(variable_name):
+        env_var = f"AIRFLOW_VAR_{variable_name.upper()}"
+        return os.getenv(env_var)
+
+    return get_airflow_variable
+
+
+@pytest.fixture()
+def real_postgres_connection():
+    dbname = os.getenv("AIRFLOW_DATABASE_NAME")
+    user = os.getenv("AIRFLOW_DATABASE_USERNAME")
+    password = os.getenv("AIRFLOW_DATABASE_PASSWORD")
+    host = os.getenv("POSTGRES_CONN_HOST")
+    port = os.getenv("POSTGRES_CONN_PORT")
+
+    conn = None
+
+    try:
+        conn = psycopg2.connect(
+            dbname=dbname,
+            user=user,
+            password=password,
+            host=host,
+            port=port
+        )
+
+        yield conn
+    except psycopg2.Error as e:
+        pytest.fail(f"Failed to connet to the database: {e}")
+
+    finally:
+        if conn:
+            conn.close()
